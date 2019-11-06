9166e126262117446b1cb05ee00f67736e144b89
DanDaika <dan.daika@softvision.ro>
2019-11-06 10:05:13 +0200

    Changes made in order to automatically generate a test report in chrome

--- conf.js
+++ conf.js
@@ -1,9 +1,24 @@
+var HtmlReporter = require('protractor-beautiful-reporter');
+var today = new Date(),
+timeStamp = '0' + today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear() + '--' + today.getHours() + 'h-' + today.getMinutes() + 'm';
+
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['./tests/tests.js'],
   multiCapabilities: [{
-    browserName: 'firefox'
+    browserName: 'chrome'
   }],
-  jasmineNodeOpts: {defaultTimeoutInterval: 60000}
+  jasmineNodeOpts: {defaultTimeoutInterval: 60000},
+  onPrepare: () => {
+    jasmine.getEnv().addReporter(new HtmlReporter({
+      baseDirectory: 'reports/'+timeStamp,
+      screenshotsSubfolder: 'images',
+      jsonsSubfolder: 'jsons',
+      excludeSkippedSpecs: true,
+      takeScreenShotsOnlyForFailedSpecs: true,
+      docTitle: 'Test Report To-Do List -- Test run on: '+timeStamp,
+      docName: 'report.html'
+    }).getJasmine2Reporter());
+  }
 }
